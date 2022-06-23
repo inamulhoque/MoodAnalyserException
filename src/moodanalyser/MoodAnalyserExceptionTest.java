fe1691d8c6f57a980d6915bdc427cf0c66900048
Inamul <inamulhoque097@gmail.com>
2022-06-23 23:12:01 +0530

    Added: Null pointer exception

--- src/moodanalyser/MoodAnalyserExceptionTest.java
+++ src/moodanalyser/MoodAnalyserExceptionTest.java
@@ -1,12 +1,17 @@
 package moodanalyser;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class MoodAnalyserExceptionTest {
+    MoodAnalyserException obj;
+    @BeforeClass
+    public void setUP(){
+        obj = new MoodAnalyserException("");
+    }
     @Test
     public void analyseMoodTest(){
-        MoodAnalyserException obj = new MoodAnalyserException("");
         String mood = obj.analyseMood();
         Assert.assertEquals("Happy",mood);
     }
