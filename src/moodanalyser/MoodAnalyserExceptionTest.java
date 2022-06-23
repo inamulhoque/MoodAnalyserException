8e37550fb4cec945a3b54ea6c9dcd7a1580ab98a
Inamul <inamulhoque097@gmail.com>
2022-06-23 18:57:47 +0530

    Added: return Happy

--- src/moodanalyser/MoodAnalyserExceptionTest.java
+++ src/moodanalyser/MoodAnalyserExceptionTest.java
@@ -6,8 +6,8 @@ import org.testng.annotations.Test;
 public class MoodAnalyserExceptionTest {
     @Test
     public void analyseMoodTest() throws Exception{
-        MoodAnalyserException obj = new MoodAnalyserException("I am in Sad mood");
+        MoodAnalyserException obj = new MoodAnalyserException("I am in Happy mood");
         String mood = obj.analyseMood();
-        Assert.assertEquals("SAD",mood);
+        Assert.assertEquals("HAPPY",mood);
     }
 }
