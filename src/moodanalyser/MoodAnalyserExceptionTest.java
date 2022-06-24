84a83455486aad57436ebaf45426ff7ada810ce0
Inamul <inamulhoque097@gmail.com>
2022-06-24 19:26:37 +0530

    Added: return SAD

--- src/moodanalyser/MoodAnalyserExceptionTest.java
+++ src/moodanalyser/MoodAnalyserExceptionTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 public class MoodAnalyserExceptionTest {
     @Test
-    public void analyseMoodTest() throws Exception{
+    public void analyseMoodTest(){
         MoodAnalyserException obj = new MoodAnalyserException();
         String mood = obj.analyseMood("I am in Sad mood");
         Assert.assertEquals("SAD", mood);
